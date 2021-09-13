8d767dbf34d72a51e58eeef9c36ea781c49046d1
Angel <84652181+xforenur@users.noreply.github.com>
2021-09-13 16:14:26 -0500

    Ejercicio 1-6

--- Lista_Doblemente_Enlazada/Double_Linked_List.h
+++ Lista_Doblemente_Enlazada/Double_Linked_List.h
@@ -14,6 +14,15 @@ class Double_Linked_List{
         Double_Linked_List(Double_Linked_List<T> &&);
         void push_back(T);
         void print();
+        double maximoIterativa();
+        double maximoRecursiva(NodoD<T>*,double );
+        void maximoRecursiva();
+        void printRecursivaInicioFin(NodoD<T>*);
+        void printRecursivaInicioFin();
+        void printRecursivaFinInicio(NodoD<T>*);
+        void printRecursivaFinInicio();
+        void printIterativaFinInicio();
+        int numPares();
         ~Double_Linked_List();
 };
 
@@ -74,6 +83,112 @@ void Double_Linked_List<T>::print(){
         cout << "[" << i << ":" << ptr->get_dato() << "]\n";
         return;
     }
+}
+
+template<typename T>
+double Double_Linked_List<T>::maximoIterativa(){
+    NodoD<T> *ptr = head;
+    int tam = size;
+    double max = 0;
+    if(head != nullptr){
+        for(int i=0; i<tam; i++){
+            if((ptr->get_dato()) > max){
+                max = ptr->get_dato();
+            }
+            ptr = ptr->get_sig();
+        }
+    }
+    return max;
+}
+
+template<typename T>
+double Double_Linked_List<T>::maximoRecursiva(NodoD<T> *nod,double max){
+    if(nod == nullptr){
+        return max;
+    }
+    else{
+        if((nod->get_dato())>= max){
+            max = nod->get_dato();
+        }
+        return maximoRecursiva(nod->get_sig(),max);
+    }
+}
+
+template<typename T>
+void Double_Linked_List<T>::maximoRecursiva(){
+    NodoD<T> *tmp = head;
+    double max=0;
+    std::cout<<maximoRecursiva(tmp,max);
+}
+
+template<typename T>
+void Double_Linked_List<T>::printRecursivaInicioFin(NodoD<T> *nod){
+    if(nod->get_sig() == nullptr){
+        std::cout<<nod->get_dato();
+    }
+    else{
+        std::cout<<nod->get_dato()<<" -> ";
+        printRecursivaInicioFin(nod->get_sig());
+    }
+}
+
+template<typename T>
+void Double_Linked_List<T>::printRecursivaInicioFin(){
+    NodoD<T> *tmp = head;
+    printRecursivaInicioFin(tmp);
+}
+
+template<typename T>
+void Double_Linked_List<T>::printRecursivaFinInicio(NodoD<T> *nod){
+    if(nod->get_ant() == nullptr){
+        printRecursivaFinInicio(nod->get_sig());
+        std::cout<<nod->get_dato();
+    }
+    else{
+        if((nod->get_sig()) != nullptr){
+            printRecursivaFinInicio(nod->get_sig());
+            std::cout<<nod->get_dato()<<" -> ";
+        }
+        else{
+            std::cout<<nod->get_dato()<<" -> ";
+        }
+    }
+}
+
+template<typename T>
+void Double_Linked_List<T>::printRecursivaFinInicio(){
+    NodoD<T> *tmp = head;
+    printRecursivaFinInicio(tmp);
+}
+
+template<typename T>
+void Double_Linked_List<T>::printIterativaFinInicio(){
+    NodoD<T> *ptr = head;
+    for(int i=1; i<size; i++){
+        ptr = ptr->get_sig();
+    }
+    for(int i=0; i<size; i++){
+        if((ptr->get_ant()) == nullptr){
+            std::cout<<ptr->get_dato();
+        }
+        else{
+            std::cout<<ptr->get_dato()<<" -> ";
+        }
+        ptr = ptr->get_ant();
+    }
+}
+
+template<typename T>
+int Double_Linked_List<T>::numPares(){
+    NodoD<T> *tmp = head;
+    int cantidad = 0;
+    for(int i=0;i<size;i++){
+        if((tmp->get_dato())%2 == 0){
+            cantidad++;
+        }
+        tmp = tmp->get_sig();
+    }
+    return cantidad;
 }
 
 template<typename T>
